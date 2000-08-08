c23025129e9516b0fa58540cb8432b2e443f89c6
menno <menno>
2000-08-08 08:35:19 +0000

    Changed for multichannel support

--- mc_enc.c
+++ mc_enc.c
@@ -1,19 +1,7 @@
-/*******************************************************************************************
- *
- * Multichannel allocation module.
- *
- * Authors:
- * CL    Chuck Lueck, TI <lueck@ti.com>
- * RG    Ralf Geiger, FhG/IIS
- *
- * Changes:
- * 22-jan-98   CL   Initial revision.
- * 07-apr-98   RG   Added determination of LFE channel.
- ***************************************************************************************/
 
 #include "mc_enc.h"
 
-void DetermineChInfo(Ch_Info* chInfo, int numChannels) {
+void DetermineChInfo(Ch_Info* chInfo, int numChannels/*, int lfePresent*/) {
    
   /* If LFE present                                                       */
   /*  Num channels       # of SCE's       # of CPE's       #of LFE's      */ 
@@ -40,45 +28,60 @@ void DetermineChInfo(Ch_Info* chInfo, int numChannels) {
   /*      2*N                2                2*(N-1)         0           */
   /*      2*N+1              1                2*N             0           */
 
-  int sceTag=0;
-  int cpeTag=0;
-  int numChannelsLeft=numChannels;
+	int sceTag=0;
+	int lfeTag=0;
+	int cpeTag=0;
+	int numChannelsLeft=numChannels;
 
- 
-  /* First element is sce, except for 2 channel case */
-  if (numChannelsLeft!=2) {
-    chInfo[numChannels-numChannelsLeft].present = 1;
-    chInfo[numChannels-numChannelsLeft].tag=sceTag++;
-    chInfo[numChannels-numChannelsLeft].cpe=0;
-    numChannelsLeft--;
-  }
+	
+	/* First element is sce, except for 2 channel case */
+	if (numChannelsLeft!=2) {
+		chInfo[numChannels-numChannelsLeft].present = 1;
+		chInfo[numChannels-numChannelsLeft].tag=sceTag++;
+		chInfo[numChannels-numChannelsLeft].cpe=0;
+		chInfo[numChannels-numChannelsLeft].lfe=0;    
+		numChannelsLeft--;
+	}
 
-  /* Next elements are cpe's */
-  while (numChannelsLeft>1) {
-    /* Left channel info */
-    chInfo[numChannels-numChannelsLeft].present = 1;
-    chInfo[numChannels-numChannelsLeft].tag=cpeTag++;
-    chInfo[numChannels-numChannelsLeft].cpe=1;
-    chInfo[numChannels-numChannelsLeft].common_window=0;
-    chInfo[numChannels-numChannelsLeft].ch_is_left=1;
-    chInfo[numChannels-numChannelsLeft].paired_ch=numChannels-numChannelsLeft+1;
-    numChannelsLeft--;
-  
-    /* Right channel info */
-    chInfo[numChannels-numChannelsLeft].present = 1;
-    chInfo[numChannels-numChannelsLeft].cpe=1;
-    chInfo[numChannels-numChannelsLeft].common_window=0;
-    chInfo[numChannels-numChannelsLeft].ch_is_left=0;
-    chInfo[numChannels-numChannelsLeft].paired_ch=numChannels-numChannelsLeft-1;
-    numChannelsLeft--;
-  }
-  
-  /* Is there another channel left ? */
-  if (numChannelsLeft) {
-      chInfo[numChannels-numChannelsLeft].present = 1;
-      chInfo[numChannels-numChannelsLeft].tag=sceTag++;
-      chInfo[numChannels-numChannelsLeft].cpe=0;
-    numChannelsLeft--;
-  }
+	/* Next elements are cpe's */
+	while (numChannelsLeft>1) {
+		/* Left channel info */
+		chInfo[numChannels-numChannelsLeft].present = 1;
+		chInfo[numChannels-numChannelsLeft].tag=cpeTag++;
+		chInfo[numChannels-numChannelsLeft].cpe=1;
+		chInfo[numChannels-numChannelsLeft].common_window=0;
+		chInfo[numChannels-numChannelsLeft].ch_is_left=1;
+		chInfo[numChannels-numChannelsLeft].paired_ch=numChannels-numChannelsLeft+1;
+		chInfo[numChannels-numChannelsLeft].lfe=0;    
+		numChannelsLeft--;
+		
+		/* Right channel info */
+		chInfo[numChannels-numChannelsLeft].present = 1;
+		chInfo[numChannels-numChannelsLeft].cpe=1;
+		chInfo[numChannels-numChannelsLeft].common_window=0;
+		chInfo[numChannels-numChannelsLeft].ch_is_left=0;
+		chInfo[numChannels-numChannelsLeft].paired_ch=numChannels-numChannelsLeft-1;
+		chInfo[numChannels-numChannelsLeft].lfe=0;
+		numChannelsLeft--;
+	}
+
+	/* Is there another channel left ?  If -lf switched then lfe else sce */
+	if (numChannelsLeft) {
+/*		if (lfePresent) { 
+			chInfo[numChannels-numChannelsLeft].present = 1;
+			chInfo[numChannels-numChannelsLeft].tag=lfeTag++;
+			chInfo[numChannels-numChannelsLeft].cpe=0;
+			chInfo[numChannels-numChannelsLeft].lfe=1; 
+		} else { */
+			chInfo[numChannels-numChannelsLeft].present = 1;
+			chInfo[numChannels-numChannelsLeft].tag=sceTag++;
+			chInfo[numChannels-numChannelsLeft].cpe=0;
+			chInfo[numChannels-numChannelsLeft].lfe=0;
+//		}
+		numChannelsLeft--;
+	} else {
+//		if (lfePresent) CommonExit(1,"no LFE channel detected");
+	}
 }
+
 
